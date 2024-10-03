35962f82bda78a311a32f587300ff6043a5ae703
Alexandre Silveira de Paula <168464775+alexandresilveiradepaula@users.noreply.github.com>
2024-10-03 16:27:05 -0300

    criação do teste do DELETE

---
+++ delete.php
@@ -0,0 +1,30 @@
+<?php
+
+spl_autoload_register(function ($classe) {
+    if (file_exists("{$classe}.class.php")) {
+        include_once "{$classe}.class.php";
+    }
+});
+
+// cria seleção de dados
+$criteria = new TCriteria;
+$criteria->add(new TFilter('id','=','3'));
+
+// cria instrução de DELETE
+$sql = new TSqlDelete;
+
+// define a entidade
+$sql->setEntity('aluno');
+
+// define o critério de seleção de dados
+$sql->setCriteria($criteria);
+
+// processa a instrução sql
+echo $sql->getInstruction();
+
+echo "<br.\n";
+
+
+
+
+?>
